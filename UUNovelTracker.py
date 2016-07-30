85767182482a99a8774c6f4d1c85ea4d691d0c88
yanadsl <yanadsl1@hotmail.com>
2016-07-30 16:52:09 +0800

    會把中間的部分一起下載了！

--- UUNovelTracker.py
+++ UUNovelTracker.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import urllib2
 import easygui
 import os
-
+from threading import Thread
 # making everything utf-8 in case of encoding error
 import sys
 
@@ -11,65 +11,24 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-# 全/半形轉換
-def str_full_to_half(textLine):
-    # adapt from https://sites.google.com/site/richchihlee/portal/python/chin-convert-1
-    myTextLine = textLine
+# using thread with this to make a non-blocking msg
+def showmsg(msg, title):
+    easygui.msgbox(msg, title)
 
-    myTextLine = myTextLine.replace("Ａ", "A")
-    myTextLine = myTextLine.replace("Ｂ", "B")
-    myTextLine = myTextLine.replace("Ｃ", "C")
-    myTextLine = myTextLine.replace("Ｄ", "D")
-    myTextLine = myTextLine.replace("Ｅ", "E")
-    myTextLine = myTextLine.replace("Ｆ", "F")
-    myTextLine = myTextLine.replace("Ｇ", "G")
-    myTextLine = myTextLine.replace("Ｈ", "H")
-    myTextLine = myTextLine.replace("Ｉ", "I")
-    myTextLine = myTextLine.replace("Ｊ", "J")
-    myTextLine = myTextLine.replace("Ｋ", "K")
-    myTextLine = myTextLine.replace("Ｌ", "L")
-    myTextLine = myTextLine.replace("Ｍ", "M")
-    myTextLine = myTextLine.replace("Ｎ", "N")
-    myTextLine = myTextLine.replace("Ｏ", "O")
-    myTextLine = myTextLine.replace("Ｐ", "P")
-    myTextLine = myTextLine.replace("Ｑ", "Q")
-    myTextLine = myTextLine.replace("Ｒ", "R")
-    myTextLine = myTextLine.replace("Ｓ", "S")
-    myTextLine = myTextLine.replace("Ｔ", "T")
-    myTextLine = myTextLine.replace("Ｕ", "U")
-    myTextLine = myTextLine.replace("Ｖ", "V")
-    myTextLine = myTextLine.replace("Ｗ", "W")
-    myTextLine = myTextLine.replace("Ｘ", "X")
-    myTextLine = myTextLine.replace("Ｙ", "Y")
-    myTextLine = myTextLine.replace("Ｚ", "Z")
-    myTextLine = myTextLine.replace("ａ", "a")
-    myTextLine = myTextLine.replace("ｂ", "b")
-    myTextLine = myTextLine.replace("ｃ", "c")
-    myTextLine = myTextLine.replace("ｄ", "d")
-    myTextLine = myTextLine.replace("ｅ", "e")
-    myTextLine = myTextLine.replace("ｆ", "f")
-    myTextLine = myTextLine.replace("ｇ", "g")
-    myTextLine = myTextLine.replace("ｈ", "h")
-    myTextLine = myTextLine.replace("ｉ", "i")
-    myTextLine = myTextLine.replace("ｊ", "j")
-    myTextLine = myTextLine.replace("ｋ", "k")
-    myTextLine = myTextLine.replace("ｌ", "l")
-    myTextLine = myTextLine.replace("ｍ", "m")
-    myTextLine = myTextLine.replace("ｎ", "n")
-    myTextLine = myTextLine.replace("ｏ", "o")
-    myTextLine = myTextLine.replace("ｐ", "p")
-    myTextLine = myTextLine.replace("ｑ", "q")
-    myTextLine = myTextLine.replace("ｒ", "r")
-    myTextLine = myTextLine.replace("ｓ", "s")
-    myTextLine = myTextLine.replace("ｔ", "t")
-    myTextLine = myTextLine.replace("ｕ", "u")
-    myTextLine = myTextLine.replace("ｖ", "v")
-    myTextLine = myTextLine.replace("ｗ", "w")
-    myTextLine = myTextLine.replace("ｘ", "x")
-    myTextLine = myTextLine.replace("ｙ", "y")
-    myTextLine = myTextLine.replace("ｚ", "z")
-    myTextLine = myTextLine.replace("．", ".")
-    return myTextLine
+
+# 全/半形轉換
+dict = {"Ａ":"A","Ｂ":"B","Ｃ":"C","Ｄ":"D","Ｅ":"E","Ｆ":"F","Ｇ":"G","Ｈ":"H","Ｉ":"I","Ｊ":"J","Ｋ":"K",
+        "Ｌ":"L","Ｍ":"M","Ｎ":"N","Ｏ":"","Ｐ":"P","Ｑ":"Q","Ｒ":"R","Ｓ":"S","Ｔ":"T","Ｕ":"U","Ｖ":"V",
+        "Ｗ":"W","Ｘ":"X","Ｙ":"Y","Ｚ":"Z","ａ":"a","ｂ":"b","ｃ":"c","ｄ":"d","ｅ":"e","ｆ":"f","ｇ":"g",
+        "ｈ":"h","ｉ":"i","ｊ":"j","ｋ":"k","ｌ":"l","ｍ":"m","ｎ":"n","ｏ":"o","ｐ":"p","ｑ":"q","ｒ":"r",
+        "ｓ":"s","ｔ":"t","ｕ":"u","ｖ":"v","ｗ":"w","ｘ":"x","ｙ":"y","ｚ":"z","．":".","（":"(","）":")"}
+def str_full_to_half(textLine):
+    key = dict.keys()
+    value = dict.values()
+    newTextLine = textLine
+    for alphabet in range(0, 54):
+        newTextLine = newTextLine.replace(key[alphabet], value[alphabet])
+    return newTextLine
 
 
 def search(Filiter, NovelName, NovelSite, LastChapter):
@@ -79,7 +38,7 @@ def search(Filiter, NovelName, NovelSite, LastChapter):
 
     # get things into Beautifulsoup
     # since what i need is on the top of the site , only input 1/4 into it
-    soup = BeautifulSoup(content[:len(content) / 4], "html.parser")
+    soup = BeautifulSoup(content, "html.parser")
 
     # find <a> tag(.a) from <div> tag with name "zuixin"(find_all) and find 1 result
     # find_all return a list , so use [0] to specify what i want
@@ -88,51 +47,78 @@ def search(Filiter, NovelName, NovelSite, LastChapter):
     # use contents to get only the content inside without <a>/</a> tag
     # check if there's a new chapter
     if a.contents[0] != LastChapter:
-        LastChapter = a.contents[0]
-        Novel = urllib2.urlopen(BaseSite + a.get('href'))
-        content = Novel.read().decode('gbk')
-        soup = BeautifulSoup(content, "html.parser")
-        soup.find_all("div", "ad_content", limit=1)[0].extract()
-        contentbox = soup.find_all("div", "contentbox", limit=1)[0].div.contents
-        s = LastChapter
-        for text in contentbox:
-            tmp = str(text).encode()
-            if tmp != "<br/>":
-                if tmp.find('flag'):
-                    s += (tmp + '\n')
+        # 抓取清單，反轉，pop掉第一個(第一章)
+        # 當抓到與上一個章節相同名稱後 find_flag = 1
+        # 紀錄後面章節的url
+        a = soup.find_all("div", "zhangjie clear", limit=1)[0].find_all("a")
+        a.reverse()
+        a.pop()
+        find_flag = 0
+        NewChapterUrlList = []
+        for s in range(0, len(a)):
+            if not find_flag:
+                if a[s].contents[0] == LastChapter:
+                    find_flag = 1
+            else:
+                LastChapter = a[s].contents[0]
+                NewChapterUrlList.append(a[s].get('href'))
 
-        # filiter
-        s = str_full_to_half(s)
-        for name in Filiter:
-            s = s.replace(name, "")
-        try:
+        # 開啟內容頁面
+        ChapterNameList = []
+        for url in NewChapterUrlList:
+            Novel = urllib2.urlopen(BaseSite + url)
+            content = Novel.read().decode('gbk')
+
+            soup = BeautifulSoup(content, "html.parser")
+            # delete div "ad_content"
+            soup.find_all("div", "ad_content", limit=1)[0].extract()
+            contentbox = soup.find_all("div", "contentbox", limit=1)[0].div.contents
+            title = soup.html.head.title.contents[0].replace("_UU看书", "")
+            s = title
+            for text in contentbox:
+                tmp = str(text).encode()
+                if tmp != "<br/>":
+                    if tmp.find('flag'):
+                        s += (tmp + '\n')
+            s = s.replace(" ", "")
+            # filiter
+            s = str_full_to_half(s)
+            for name in Filiter:
+                s = s.replace(name, "")
             # get desktop path
             # from   http://wiki.alarmchang.com/index.php?title=Python_使用環境變數讀取Desktop_桌面路徑
             desktop = os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], "Desktop")
-            FileName = (LastChapter.replace(" ", "") + ".txt").encode().decode()
-            with open(desktop+FileName, 'w') as f:
-                f.write(s)
-                easygui.msgbox(NovelName + "更新啦！\n" + LastChapter, "就跟你說去看小說啦")
-                # openfile
-                try:
-                    os.startfile(desktop + FileName)
-                except:
-                    easygui.msgbox("無法開啟下載檔案", "QQ請找作者回報")
-                return LastChapter
-        except:
-            easygui.msgbox("無法建造檔案", "QQ請找作者回報")
+            try:
+                FileName = (title.replace(" ", "") + ".txt").encode().decode()
+                with open(desktop + "\\" + FileName, 'w') as f:
+                    f.write(s)
+                    ChapterNameList.append(title)
+            except:
+                easygui.msgbox("無法建造檔案", "QQ請找作者回報")
+
+        Thread(target=showmsg, args=(NovelName + "更新啦！\n" + '\n'.join(ChapterNameList), u"就跟你說去看小說啦")).start()
+        #easygui.msgbox(NovelName + "更新啦！\n" + '\n'.join(ChapterNameList), "就跟你說去看小說啦")
+        # openfile
+        # 先開的視窗(舊章節)會被蓋在下面，故用reverse
+        ChapterNameList.reverse()
+        for name in ChapterNameList:
+            try:
+                os.startfile((desktop + "\\" + name.replace(" ", "") + ".txt").encode().decode())
+            except:
+                easygui.msgbox("無法開啟下載檔案", "QQ請找作者回報")
+        return LastChapter
     else:
         print "nothing"
+
 
 # Main Entry Point
 try:
     flag = 0
     f = open("config.txt", "r")
     fread = f.read()
-    print fread
     a = fread.split("\"")
     quantity = (len(a) - 2) / 6
-    print "一共" + str(quantity) + "本書要找"
+    print str(quantity) + " Books to find"
     for things in a:
         if not things.find("Filiter"):
             Filiter = things[8:]
@@ -179,10 +165,9 @@ except:
             if fieldValues[i].strip() == "":
                 errmsg += '"%s" 不能為空.\n' % fieldNames[i]
         if errmsg == "": break  # no problems found
-        fieldValues = easygui.multenterbox(msg+"\n"+errmsg, title, fieldNames, fieldValues)
+        fieldValues = easygui.multenterbox(msg + "\n" + errmsg, title, fieldNames, fieldValues)
     with open("config.txt", "w") as f:
         f.write("\"Filiter:" + fieldValues[0] + "\"\n"
                 "\"NovelName:" + fieldValues[1] + "\"\n"
                 "\"NovelSite:" + fieldValues[2] + "\"\n"
                 "\"LastChapter:" + fieldValues[3] + "\"")
-    print "Reply was:", fieldValues
